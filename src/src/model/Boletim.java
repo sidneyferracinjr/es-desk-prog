6fe69627b5c0f781e164f62e307295e1db338335
sidneyferracinjr <sidneyferracinjr@gmail.com>
2024-11-30 20:34:58 -0300

    start the project

---
+++ src/src/model/Boletim.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author sidne
+ */
+public class Boletim implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private static int contaAluno = 1;
+    private int idBoletim;
+    private String aluno;
+    private float media;
+
+    public Boletim(String aluno, float media) {
+        this.aluno = aluno;
+        this.media = media;
+    }
+    
+    public Boletim(String aluno) {
+        this.aluno = aluno;
+    }
+    
+    public Boletim() {
+        idBoletim = contaAluno;
+        contaAluno++;
+    }
+
+    @Override
+    public String toString() {
+        return "Boletim{" + "aluno=" + aluno + ", media=" + media + '}';
+    }
+
+    public int getIdBoletim() {
+        return idBoletim;
+    }
+
+    public void setIdBoletim(int idBoletim) {
+        this.idBoletim = idBoletim;
+    }  
+  
+    public String getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(String aluno) {
+        this.aluno = aluno;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    public void setMedia(float media) {
+        this.media = media;
+    }
+    
+}
